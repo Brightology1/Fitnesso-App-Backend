22754b7a4736e0ca92fa92f2fc8bdaf0c5a7d39f
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-02-13 22:36:12 +0100

    Bug fix and correction

--- src/main/java/com/decagon/fitnessoapp/service/PersonService.java
+++ src/main/java/com/decagon/fitnessoapp/service/PersonService.java
@@ -19,13 +19,13 @@ public interface PersonService {
 
     ResponseEntity<String> removeTrainer(Long id);
 
-    void sendingEmail(PersonRequest personRequest) throws MailjetSocketTimeoutException, MailjetException;
+    PersonResponse sendingEmail(String email) throws MailjetSocketTimeoutException, MailjetException;
 
     ChangePasswordResponse updateCurrentPassword(ChangePasswordRequest changePasswordRequest);
 
-    String resetPasswordToken(String email) throws MailjetSocketTimeoutException, MailjetException;
+    PersonResponse resetPasswordToken(String email) throws MailjetSocketTimeoutException, MailjetException;
 
-    String updateResetPassword(ResetPasswordRequest passwordRequest, String token);
+    PersonResponse updateResetPassword(ResetPasswordRequest passwordRequest, String token);
 
     void resetPasswordMailSender(String email, String token) throws MailjetSocketTimeoutException,
             MailjetException;
