ac0fa84cd7f402f1e4347d74ed5071e01910d08c
Ellaboye <92469051+Ellaboye@users.noreply.github.com>
2022-03-17 14:34:26 +0100

    Update ContactServiceImpl.java

--- src/main/java/com/decagon/fitnessoapp/service/serviceImplementation/ContactServiceImpl.java
+++ src/main/java/com/decagon/fitnessoapp/service/serviceImplementation/ContactServiceImpl.java
@@ -5,21 +5,23 @@ import com.decagon.fitnessoapp.dto.transactionDto.ContactRequest;
 import com.decagon.fitnessoapp.model.user.Contact;
 import com.decagon.fitnessoapp.repository.ContactRepository;
 import com.decagon.fitnessoapp.service.ContactService;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ContactServiceImpl implements ContactService {
     private final ContactRepository contactRepository;
-    private ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
 
     @Override
     public ContactRequest save(ContactRequest contactRequest) {
-        contactRepository.save(modelMapper.map(contactRequest, Contact.class));
+        Contact contact = new Contact();
+        modelMapper.map(contactRequest, contact);
+        contactRepository.save(contact);
         return contactRequest;
     }
 
